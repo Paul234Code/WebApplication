85c51ea0119a799cab7ff5cb86e5b86ab1b4d785
R1886510Paul <r1886510@csfoy.ca>
2021-11-17 01:02:47 -0500

    modification #9

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // javascript file for web page
+let liste = document.querySelectorAll("#formulaire input");
+for (let i = 0; i < liste.length;i++) {
+    liste[i].addEventListener('click',function (){
+        liste[i].style.border = "1px solid blue";
+    })
+
+}
