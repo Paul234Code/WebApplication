38994b120e41d84d219faeb7a6d53c5d53504aaf
Paul234Code <fayepaul234@gmail.com>
2021-11-23 11:07:30 -0500

    modification #11

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // javascript file for web page
 let liste = document.querySelectorAll("#formulaire input");
 for (let i = 0; i < liste.length;i++) {
-    liste[i].addEventListener('click',function (){
-        liste[i].style.border = "1px solid blue";
+    liste[i].addEventListener('focus',function (){
+        liste[i].style.border = "2px solid #1E90FF";
     })
 
 }
