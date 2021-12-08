b876a33617961b9d202854e975f5456e6396c9e4
Paul Faye <64893580+Paul234Code@users.noreply.github.com>
2021-12-08 15:44:47 -0500

    add html files

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ for (let i = 0; i < liste.length;i++) {
     })
 
 }
+let label1 =  document.getElementById("label1");
+label12 = document.createElement('label');
+label12.textContent = "-";
+label12.style.size = "15px"
+label12.style.color = "#f1f1f1";
+label12.style.width = "20px";
+let para1 = document.getElementById("para1");
+let  text = document.createTextNode("If the design work in your mind it works in your heart");
+let paragraph = document.createElement('p');
+paragraph.appendChild(text);
+label1.addEventListener('click',function () {
+    para1.innerHTML = paragraph.innerHTML;
+    label1.textContent = label12.textContent;
+});
